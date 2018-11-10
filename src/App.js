1087a80f3121eb8e5ae26549aeed2ca6f9676900
Nital Patel <patelnital36@gmail.com>
2018-11-10 13:30:47 -0500

    final setup

--- src/App.js
+++ src/App.js
@@ -4,25 +4,12 @@ import './App.css';
 import Header from './component/header/Header';
 import Gallery from './component/Gallery';
 
-import createBrowserHistory from 'history/createBrowserHistory'
-
-const history = createBrowserHistory();
-window.applicationHistory = history;
-
-
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            user: null
-        };
-    }
     render() {
         return (
-                <div className="App">
+                <div>
                     <Header/>
-                        <div className="content">
+                        <div>
                             <Gallery/>
                         </div>
                 </div>
