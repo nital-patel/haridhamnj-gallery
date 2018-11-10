80cb9c8257128d50f41077942b986362e8bca634
Nital Patel <patelnital36@gmail.com>
2018-11-10 13:19:22 -0500

    final setup

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -5,13 +5,27 @@ class Gallery extends Component {
         super();
         this.state = {
             imageList: [
-                'https://events.yds.org/images/hms-hero.jpg',
-                'https://events.yds.org/images/hms-hero.jpg',
-                'https://events.yds.org/images/hms-hero.jpg',
-                'https://events.yds.org/images/hms-hero.jpg',
-                'https://events.yds.org/images/hms-hero.jpg',
-                'https://events.yds.org/images/hms-hero.jpg',
-                'https://events.yds.org/images/hms-hero.jpg'
+                'https://haridhamnj.org/wp-content/uploads/2014/09/AmbrishDiksha-01.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2018/08/AYS2018-46.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2018/09/RakshaBandhanNJ-27.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2018/09/RakshaBandhanNJ-30.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2018/07/Youth-Sabha-10.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2018/07/Youth-Sabha-12.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2018/07/Youth-Sabha-13.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2018/07/GuruhariSwamiji_Airport-Arrival-19.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2018/07/GuruhariSwamiji_Airport-Arrival-18.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2018/07/GuruhariSwamiji_Airport-Arrival-6.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2018/04/SMSGD_18-1-1030x659.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2014/09/Gurupoornima2014-16.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2014/09/Gurupoornima2014-53.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2014/09/Gurupoornima2014-56.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2014/09/Gurupoornima2014-11.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2014/09/Gurupoornima2014-14.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2014/09/GBM_Session-4-05.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2014/09/GBM_Session-4-17.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2014/09/GBM_Session-4-19.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2014/09/AmbrishDiksha-15.jpg',
+                'https://haridhamnj.org/wp-content/uploads/2014/09/AmbrishDiksha-18.jpg'
             ]
         };
     }
